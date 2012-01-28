b867651674cd192c1f66ad26e8a694c4f7eb2f1a
J Brony <mljbrony@gmail.com>
2012-01-28 13:01:09 -0500

    Added Google Analytics tracking code to redditwide.php

--- redditwide.php
+++ redditwide.php
@@ -127,6 +127,19 @@ else {
 <link rel="stylesheet" href="assets/redditwide.css">
 <script src="assets/mootools.min.js"></script>
 <script src="assets/redditwide.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28726533-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 <div class="container">
